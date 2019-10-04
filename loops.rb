15381391dffa2be5e31c9bcc0d21c432f7f03181
Sanjana Sankholkar <sanjan.sankholkar@gmail.com>
2019-10-04 09:23:44 -0600

    Create loops

---
+++ loops.rb
@@ -0,0 +1,106 @@
+# done = false
+
+# while !done
+#   puts "Lol"
+# end
+
+# puts "Give me your age"
+# $stdout.flush
+# age = gets.chomp.to_i
+
+# while age <0
+#   puts "That's not a real age, dummy. Try again"
+#   $stdout.flush
+#   age = gets.chomp.to_i
+# end
+
+# i = 0
+# while i < 30
+#   puts "Hi"
+
+#   i = i + 1
+# end
+
+# 3.times do
+#   puts "Hi"
+# end
+
+# i = 0
+# x = "hello"
+# x.size.times do
+#   puts x[i]
+#   i = i + 1
+# end
+
+# i = 0
+# while i<100
+#   puts i
+
+#   i = i + 2
+# end
+
+# i = 0
+# 50.times do
+#   puts i
+#   i = i + 2
+# end
+
+# i = 0
+# 10.times do
+#   puts i * i
+#   i = i + 1
+# end
+
+#       0  1  2  3  4
+# nums = [3, 5, 4, 3, 5]
+# letters = ["a", "b", "c", "d", "e"]
+# prices = [5.99, 2.00, 0.99]
+
+# puts nums[0] #3
+# puts nums[3] #3
+# puts nums[18] #nothing
+# puts nums[-6] #nothing
+# puts nums [-3] #counts backwards from end, so 4
+# puts nums[nums.size - 2] #3
+# puts nums[nums.size] #nothing because it goes beyond the list
+
+# i = 0
+# while i < nums.size
+#   puts nums[i]
+#   i = i + 1
+# end
+
+# nums = [3, 5, 4, 3, 5, 3, 3, 3]
+
+# nums.each do |num| #can be any name
+#   puts num
+# end
+
+# count = 0
+# nums.each do |num|
+#   if num == 3
+#     count = count + 1
+#   end
+# end
+
+# puts " Count: #{count}"
+
+# total = 0
+# nums.each do |num|
+#   total = total + num
+# end
+
+# puts total
+
+# puts nums
+
+# print nums, "\n"
+
+# puts "Hi"
+
+# x = "abcdef"
+# i = 0
+# x.size.times do |i|
+#   puts x[i]
+#   i = i + 1
+# end
